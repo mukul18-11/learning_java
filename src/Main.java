1551a41b6e60f51c8f32ba1efad9efd52d42d675
Mukul Kumar <letsstartcode@gmail.com>
2025-11-18 18:23:41 +0530

    new-changes-arr

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = new int[5];
+
+        System.out.println(arr.length);
 
 
     }
